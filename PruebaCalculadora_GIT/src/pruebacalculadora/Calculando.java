901053bf6aa941c85903078e1ffa6eaabdadc1bf
ptorluj <ptorluj539@g.educaand.es>
2024-05-08 19:58:38 +0200

    Add files via upload

---
+++ PruebaCalculadora_GIT/src/pruebacalculadora/Calculando.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pruebacalculadora;
+
+/**
+ *
+ * @author usuario
+ */
+public class Calculando {
+
+    public double add(double number1, double number2){
+    return number1 + number2; 
+    } 
+    
+    public double subtract(double number1, double number2){ 
+    return number1 - number2; 
+    } 
+    
+    public double multiply(double number1, double number2) { 
+    return number1 * number2; 
+    } 
+
+    public double divide(double number1, double number2){
+        if(number2==0)
+            throw new ArithmeticException("divisi?n por cero");
+    return number1 / number2; 
+}
+
+
+
+
+}
